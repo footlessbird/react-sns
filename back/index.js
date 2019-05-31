fbff2b8e530d353e870d51f4c6d35bb3157a482d
footlessbird <>
2019-05-31 15:31:20 -0700

    singup controller

--- back/index.js
+++ back/index.js
@@ -9,6 +9,10 @@ const app = express();
 
 db.sequelize.sync();
 
+//  req.body를 사용하기 위해
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use("/api/user", userAPIRouter);
 app.use("/api/post", postAPIRouter);
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,44 +1,48 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const bcrypt = require("bcrypt");
+const db = require("../models");
+const router = express.Router();
 
-router.post('/',(req,res)=>{
+router.get("/", (req, res) => {});
 
-})
+router.post("/", async (req, res) => {
+  try {
+    const userExists = await db.User.findOne({
+      where: {
+        userId: req.body.userId
+      }
+    });
+    if (userExists) {
+      return res.status(403).send("The ID already exists");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 12);
+    const newUser = await db.User.create({
+      nickname: req.body.nickname,
+      userId: req.body.userId,
+      password: hashedPassword
+    });
+    console.log(newUser);
+    return res.json(newUser);
+  } catch (e) {
+    console.log(e);
+    return next(e)
+  }
+});
 
-router.post('/',(req,res)=>{
+router.get("/:id", (req, res) => {});
 
-})
+router.post("/logout", (req, res) => {});
 
-router.get('/:id',(req,res)=>{
+router.post("/login", (req, res) => {});
 
-})
+router.get("/:id/follow", (req, res) => {});
 
-router.post('/logout',(req,res)=>{
+router.post("/:id/follow", (req, res) => {});
 
-})
+router.delete("/:id/follow", (req, res) => {});
 
-router.post('/login',(req,res)=>{
+router.delete("/:id/follower", (req, res) => {});
 
-})
+router.get("/:id/posts", (req, res) => {});
 
-router.get('/:id/follow',(req,res)=>{
-
-})
-
-router.post('/:id/follow',(req,res)=>{
-
-})
-
-router.delete('/:id/follow',(req,res)=>{
-
-})
-
-router.delete('/:id/follower',(req,res)=>{
-
-})
-
-router.get('/:id/posts',(req,res)=>{
-
-})
-
-module.exports = router
+module.exports = router;
