86ae5e15df78e6730b593716b98e0898efb5b36c
footlessbird <>
2019-05-31 12:23:50 -0700

    back-end api

--- back/index.js
+++ back/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 
+const db = require("./models");
+const userAPIRouter = require("./routes/user"); // 공통된 부분을 따로 빼냄 모듈화? /api/user
+const postAPIRouter = require("./routes/post");
+const postsAPIRouter = require("./routes/posts");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Yo server~");
-});
+db.sequelize.sync();
+
+// API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
+app.use("/api/user", userAPIRouter);
+app.use("/api/post", postAPIRouter);
+app.use("/api/posts", postsAPIRouter);
 
 app.listen(3306, () => {
   console.log(`Your server is runnin' on localhost:3306`);
---
+++ back/models/index.js
@@ -0,0 +1,37 @@
+const Sequelize = require("sequelize");
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
+
+db.Comment = require("./comment")(sequelize, Sequelize);
+db.Hashtag = require("./hashtag")(sequelize, Sequelize);
+db.Image = require("./image")(sequelize, Sequelize);
+db.Post = require("./post")(sequelize, Sequelize);
+db.User = require("./user")(sequelize, Sequelize);
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
