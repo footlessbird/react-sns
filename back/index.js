a9084713e9edd5d40934eb9a5e360cd16e82f666
footlessbird <>
2019-05-31 17:36:59 -0700

    login setup

--- back/index.js
+++ back/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const dotenv = require('dotenv')
 
 const db = require("./models");
 const userAPIRouter = require("./routes/user"); // 공통된 부분을 따로 빼냄 모듈화? /api/user
 const postAPIRouter = require("./routes/post");
 const postsAPIRouter = require("./routes/posts");
 
+dotenv.config()
 const app = express();
 
 db.sequelize.sync();
@@ -16,8 +20,19 @@ app.use(morgan("dev"));
 //  req.body를 사용하기 위해
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(cors())
+app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false, // 매번 세션 강제 저장
+    saveUninitialized: false, // 빈 값도 저장
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false // https를 쓸 때 true
+    }
+  })
+);
 
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use("/api/user", userAPIRouter);
