7a7a3a5888274ae774748e185581239b348ae4f7
footlessbird <>
2019-07-01 18:23:16 -0700

    a back-end route added

--- back/index.js
+++ back/index.js
@@ -40,6 +40,11 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res)=>{
+  res.send('react-sns back-end successfully working');
+
+})
+
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
