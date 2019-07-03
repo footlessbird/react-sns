4fa6226a6ff2921af9f99f8c63c6654dbc88d089
footlessbird <>
2019-07-03 15:36:01 -0700

    bucket name changed properly

--- back/routes/post.js
+++ back/routes/post.js
@@ -18,7 +18,7 @@ AWS.config.update({
 const upload = multer({
   storage: multerS3({
     s3: new AWS.S3(),
-    bucket: 'react-nodebird',
+    bucket: 'react-sns',
     key(req, file, cb) {
       cb(null, `original/${+new Date()}${path.basename(file.originalname)}`);
     },
